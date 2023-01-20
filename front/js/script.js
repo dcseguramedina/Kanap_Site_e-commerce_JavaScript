043241926aaa3c1671ed0b61c7017d204fa1215c
Catalina <dseguramedina@gmail.com>
2023-01-20 13:16:42 +0100

    Insert products into the homepage

---
+++ front/js/script.js
@@ -0,0 +1,53 @@
+const url = 'http://localhost:3000/api/products/';
+
+//Request the API to get the set of products
+fetch(url)
+    .then(function (response) {
+        //Check the URL and retrieve the response in the json format
+        if (response.ok) {
+            return response.json();
+        }
+    })
+    .then(function (products) {
+        //Browse the response to insert each product in the homepage (DOM)
+        for (let product of products) {
+
+            //Recover the DOM element that will host the products 
+            const sectionItems = document.getElementById("items");
+            
+            //Create an "a" tag to make the link to a product
+            const linkElement = document.createElement("a");
+            //Set the "a" tag and its "href" attribute
+            linkElement.href = `./product.html?id=${product._id}`;
+            //Attach the "a" tag to the items section
+            sectionItems.appendChild(linkElement);
+
+            //Create an "article" tag dedicated to a product
+            const productElement = document.createElement("article");
+            //Attach the "article" tag to the link section
+            linkElement.appendChild(productElement);
+            
+            //Create an "img" tag for each product
+            const imageElement = document.createElement("img");
+            imageElement.src = product.imageUrl;
+            imageElement.alt = product.altTxt;
+            //Attach the "img" tag of each product to the article section
+            productElement.appendChild(imageElement);
+            
+            //Create a "h3" tag for each product
+            const nameElement = document.createElement("h3");
+            nameElement.textContent = product.name;
+            //Attach the "h3" tag of each product to the article section
+            productElement.appendChild(nameElement);
+            
+            //Create a "p" tag for each product
+            const descriptionElement = document.createElement("p");
+            descriptionElement.textContent = product.description;
+            //Attach the "p" tag of each product to the article section
+            productElement.appendChild(descriptionElement);  
+        }
+    })
+    .catch(function (error) {
+        //Block of code to handle errors
+        return error;
+    })
