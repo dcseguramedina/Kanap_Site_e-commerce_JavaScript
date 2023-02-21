38dabba8b7553a4f3435f3a2943c4415193d2859
Catalina <dseguramedina@gmail.com>
2023-02-21 23:05:37 +0100

    confirmation: code cleanup

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -1,15 +1,23 @@
+//Display orderId - This number must not be kept or stored//
+function initConfirmation() {
+    displayOrderId();
+}
+initConfirmation();
 
-function displayOrderId() {
-    //Get the order id of the product to display
+//Get the orderId and display it in the confirmation page
+function getOrderIdToDisplay() {
     const str = window.location.href;
     const url = new URL(str);
-    let orderId = url.searchParams.get("id");
+    return url.searchParams.get("id");
+}
 
-    //Recover the DOM element that will host the order Id
+//Recover the DOM element and display the orderId
+function displayOrderId() {
+    let orderId = getOrderIdToDisplay();
+    //Recover the DOM element containing the order Id
     const confirmation = document.getElementById("orderId");
-
-    //Display the order Id
+    //Display the orderId in the page
     confirmation.textContent = orderId;
 }
 
-displayOrderId(orderId)
+initProduct();   
