c6a9b9cb1a170d66969d447a3d7b132b4a016bf4
Catalina <dseguramedina@gmail.com>
2023-02-22 11:02:09 +0100

    Confirmation: add thanks message

--- front/js/confirmation.js
+++ front/js/confirmation.js
@@ -4,20 +4,18 @@ function initConfirmation() {
 }
 initConfirmation();
 
-//Get the orderId and display it in the confirmation page
+//Get the orderId to display in the confirmation page
 function getOrderIdToDisplay() {
     const str = window.location.href;
     const url = new URL(str);
     return url.searchParams.get("id");
 }
 
-//Recover the DOM element and display the orderId
+//Recover the DOM element and insert the orderId to display
 function displayOrderId() {
     let orderId = getOrderIdToDisplay();
     //Recover the DOM element containing the order Id
     const confirmation = document.getElementById("orderId");
     //Display the orderId in the page
-    confirmation.textContent = orderId;
-}
-
-initProduct();   
+    confirmation.textContent = orderId + `. Merci pour votre commande`;
+}  
